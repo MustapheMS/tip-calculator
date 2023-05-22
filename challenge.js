c229de4f3af85e0dc6ec3dac68755647d1aae01e
MustapheMS <moztaphem9@gmail.com>
2023-05-22 11:01:13 +0300

    Tip Calculator Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,16 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+const bill = 275;
+let tip;
+
+if (bill >= 50 && bill <= 300) {
+  tip = bill * 0.15;
+} else {
+  tip = bill * 0.2;
+}
+
+const totalValue = bill + tip;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${totalValue}`);
